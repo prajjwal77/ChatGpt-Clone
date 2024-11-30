c3ac8fde95a21fdcf037432b38e9b768a4ee4e3d
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2024-11-30 22:40:13 +0530

    Update ChatMessage.java

--- ChatGpt-Clone/src/main/java/com/ChatGptClone/Model/ChatMessage.java
+++ ChatGpt-Clone/src/main/java/com/ChatGptClone/Model/ChatMessage.java
@@ -6,17 +6,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-public class ChatMessage {
-		@Id
+public class ChatHistory {
+
+	   	@Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private Long id;
 
 	    @ManyToOne
-	    @JoinColumn(name = "user_id")
 	    private User user;
 
 	    private String message;
