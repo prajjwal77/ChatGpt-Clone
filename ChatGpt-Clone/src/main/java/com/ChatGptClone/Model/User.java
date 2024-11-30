15c91651003b31cff3a8ce47d9c371af45a8c797
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2024-11-30 22:39:48 +0530

    Update User.java

--- ChatGpt-Clone/src/main/java/com/ChatGptClone/Model/User.java
+++ ChatGpt-Clone/src/main/java/com/ChatGptClone/Model/User.java
@@ -1,67 +1,60 @@
 package com.ChatGptClone.Model;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 
 @Entity
 public class User {
-	    @Id
+
+	 	@Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private Long id;
-	    private String username;
-	    private String password;
-	    private String role; // "USER" or "ADMIN"
 
-	    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-	    private List<ChatMessage> chatMessages;
+	    private String email;
+	    private String mobile;
+	    private String password;
+	    private String otp;
+	    private boolean verified;
 	    
-	    // Getters and Setters
 		public Long getId() {
 			return id;
 		}
-
 		public void setId(Long id) {
 			this.id = id;
 		}
-
-		public String getUsername() {
-			return username;
+		public String getEmail() {
+			return email;
 		}
-
-		public void setUsername(String username) {
-			this.username = username;
+		public void setEmail(String email) {
+			this.email = email;
 		}
-
+		public String getMobile() {
+			return mobile;
+		}
+		public void setMobile(String mobile) {
+			this.mobile = mobile;
+		}
 		public String getPassword() {
 			return password;
 		}
-
 		public void setPassword(String password) {
 			this.password = password;
 		}
-
-		public String getRole() {
-			return role;
+		public String getOtp() {
+			return otp;
 		}
-
-		public void setRole(String role) {
-			this.role = role;
+		public void setOtp(String otp) {
+			this.otp = otp;
 		}
-
-		public List<ChatMessage> getChatMessages() {
-			return chatMessages;
+		public boolean isVerified() {
+			return verified;
 		}
-
-		public void setChatMessages(List<ChatMessage> chatMessages) {
-			this.chatMessages = chatMessages;
+		public void setVerified(boolean verified) {
+			this.verified = verified;
 		}
-
-	  
 	    
+	    
+
 }
